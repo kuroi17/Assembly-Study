eeeae6bc02704ed43970ae70bc86af1d4daea933
Jyvhan Earl Ponce <140694543+kuroi17@users.noreply.github.com>
2025-11-18 20:37:26 +0000

    2task

---
+++ task2_arithmetic.asm
@@ -0,0 +1,25 @@
+section .data
+    result  dd 0          ; Variable to store the final result
+
+
+    section .text
+        global _start
+
+        _start:
+            ; Load numbers
+                mov eax, 6
+                    mov ebx, 3
+
+                        ; Compute (eax * ebx) + 10 - 4
+                            mul ebx
+                                add eax, 10
+                                    sub eax, 4
+
+                                        ; Store result in memory
+                                            mov [result], eax
+
+                                                ; Exit
+                                                    mov eax, 60
+                                                        xor edi, edi
+                                                            syscall
+                                                            
